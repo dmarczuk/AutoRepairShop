8d21c1798bad647936e8ad437595f59152fe6077
MateuszMoskal <mateusz.moskal07@wp.pl>
2024-10-20 13:13:50 +0200

    first commit

---
+++ src/main/java/com/example/warsztat_samochodowy/WarsztatSamochodowyApplication.java
@@ -0,0 +1,13 @@
+package com.example.warsztat_samochodowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsztatSamochodowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarsztatSamochodowyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/warsztat_samochodowy/WarsztatSamochodowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warsztat_samochodowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarsztatSamochodowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
